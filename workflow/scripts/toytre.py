f8cc8c8b9754d02278d4a2815776213df5f57905
Dave Lunt <dave.lunt@gmail.com>
2025-07-30 11:36:07 +0100

    mapping dictionary for colours

--- workflow/scripts/toytre.py
+++ workflow/scripts/toytre.py
@@ -1,88 +1,73 @@
-# Draw trees using the toytree package
-# ------------------------------------
-
 import toytree
 
-# get input tree files from snakemake
+# Get input tree files from Snakemake
 newick = snakemake.input.nwk
-
-#  get a list of newly-added tipnames
 newtips_list = snakemake.input.added
-# newick = "results/fasttree/2012_114_mafft_trimal_fasttree.nwk"
-
-# outfile, html format
 outfile = snakemake.output[0]
-# outfile = "results/reporting/toytree/2012_114_mafft_trimal_fasttree_midpoint.html"
 
-# Load the tree from file
+# Load and re-root the tree
 with open(newick) as f:
     tree1 = toytree.tree(f.read())
-
-# re-root on internal edge selected using a regex string
 rtree = tree1.root("~Pratylenchus")
 
-# Load tip names of newly-added-sequences from text file
-tips_to_mark = set()
+# Load tip names of newly-added sequences
 with open(newtips_list) as f:
-    for line in f:
-        tip = line.strip()
-        if tip:  # Skip empty lines
-            tips_to_mark.add(tip)
+    tips_to_mark = {line.strip() for line in f if line.strip()}
 
-colorlist = ["#0675b9" if "javanica" in tip
-             else "#f205cb" if "floridensis" in tip
-             else "#049e28" if "arenaria" in tip
-             else "#f90505" if "incognita" in tip
-             else "#aa11f1" if "haplanaria" in tip
-             else "#cd6df9" if "ethiopica" in tip
-             else "#e7c2f8" if "konaensis" in tip
-             else "#c9b2d4" if "arabicida" in tip
-             else "#a78fb3" if "paranaensis" in tip
-             else "#bd8ed2" if "hispanica" in tip
-             else "#dbb6ed" if "luci" in tip
-             else "#f16435" if "enterolobii" in tip
-             else "#c85490" if "hapla" in tip
-             else "#d6557c" if "partityla" in tip
-             else "#f2a6b0" if "microtyla" in tip
-             else "#f2c6d4" if "spartelensis" in tip
-             else "#f2729a" if "dunensis" in tip
-             else "#f6477e" if "duytsi" in tip
-             else "#7bbbe3" if "graminicola" in tip
-             else "#83cdfa" if "oryzae" in tip
-             else "#a6cde5" if "kralli" in tip
-             else "#70a7c9" if "naasi" in tip
-             else "#1181c7" if "minor" in tip
-             else "#5384a3" if "chitwoodi" in tip
-             else "#5ea9d7" if "fallax" in tip
-             else "#33373a" for tip in rtree.get_tip_labels()]
+# Mapping of species substrings to colors
+species_colors = {
+    "javanica": "#0675b9",
+    "floridensis": "#f205cb",
+    "arenaria": "#049e28",
+    "incognita": "#f90505",
+    "haplanaria": "#aa11f1",
+    "ethiopica": "#cd6df9",
+    "konaensis": "#e7c2f8",
+    "arabicida": "#c9b2d4",
+    "paranaensis": "#a78fb3",
+    "hispanica": "#bd8ed2",
+    "luci": "#dbb6ed",
+    "enterolobii": "#f16435",
+    "hapla": "#c85490",
+    "partityla": "#d6557c",
+    "microtyla": "#f2a6b0",
+    "spartelensis": "#f2c6d4",
+    "dunensis": "#f2729a",
+    "duytsi": "#f6477e",
+    "graminicola": "#7bbbe3",
+    "oryzae": "#83cdfa",
+    "kralli": "#a6cde5",
+    "naasi": "#70a7c9",
+    "minor": "#1181c7",
+    "chitwoodi": "#5384a3",
+    "fallax": "#5ea9d7"
+}
 
+# Function to get color based on species name
+def get_color(tip):
+    return next((color for species, color in species_colors.items() if species in tip), "#33373a")
 
-# draw a plot and store the Canvas object to a variable
+# Generate color list using list comprehension
+colorlist = [get_color(tip) for tip in rtree.get_tip_labels()]
+
+# Draw tree
 canvas, axes, mark = rtree.draw(
-    return_axes=True,  # Return the matplotlib axes object
+    return_axes=True,
     width=800,
     height=1600,
-    node_hover=True, 
-    node_sizes=4, 
-    # tip_labels_align=True,
+    node_hover=True,
+    node_sizes=4,
     tip_labels_colors=colorlist,
-    );
+)
 
-# # Add markers to the specified tips
+# Optional: Add markers to newly added tips
 # mark2 = rtree.annotate.add_tip_markers(
 #     axes=axes,
 #     tipnames=tips_to_mark,
 #     size=8,
 #     color="red",
-#     marker="o")
+#     marker="o"
+# )
 
-# save annotated tree to HTML file
+# Save to HTML
 toytree.save(canvas, outfile)
-
-
-# https://eaton-lab.org/toytree/draw-options/#tip_labels_colors
-# make list of hex color values based on tip labels
-# colorlist = ["#d6557c" if "rex" in tip else "#5384a3" for tip in rtre.get_tip_labels()]
-# rtre.draw(
-#     tip_labels_align=True, 
-#     tip_labels_colors=colorlist
